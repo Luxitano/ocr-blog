51217c0be4ab85a93cbc9c54b63d064cdb8d1aab
Philippe Pinheiro <philippe.pinheiro@list.lu>
2019-08-27 16:18:30 +0200

    use firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,19 @@ export class AppComponent {
 
   title = 'ocr';
 
+  constructor(){
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCxQa7116n1OguUHFc-gfi2Tmo-GkOjKZo",
+      authDomain: "ocrblog.firebaseapp.com",
+      databaseURL: "https://ocrblog.firebaseio.com",
+      projectId: "ocrblog",
+      storageBucket: "ocrblog.appspot.com",
+      messagingSenderId: "981673464202",
+      appId: "1:981673464202:web:5f6f32899fc1d002"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,17 +1,21 @@
 import {Post} from "../models/post.model";
 import {Injectable} from "@angular/core";
 import {Subject} from "rxjs";
+import * as firebase from 'firebase';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Injectable()
 export class PostsService {
 
+  defaultPosts = []; //If database is empty, store defaults posts
   posts = [];
   postsSubject = new Subject<Post[]>();
 
   constructor(){
-    this.posts.push(new Post("Mon premier post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"));
-    this.posts.push(new Post("Mon deuxième post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"));
-    this.posts.push(new Post("Encore un post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"));
+    //this.defaultPosts.push(new Post("Mon premier post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"));
+    //this.defaultPosts.push(new Post("Mon deuxième post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis"));
+    //this.defaultPosts.push(new Post("Encore un post","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"));
+    this.getPosts();
   }
 
   emitPosts(){
@@ -20,18 +24,20 @@ export class PostsService {
 
   createNewPost(post:Post){
     this.posts.push(post);
+    this.savePosts();
     this.emitPosts();
   }
 
   removePost(postId:number){
     this.posts.splice(postId,1);
+    this.savePosts();
     this.emitPosts();
   }
-
 
   onLoveIt(postId:number) {
     if(this.posts[postId]) {
       this.posts[postId].loveIts++;
+      this.savePosts();
       this.emitPosts();
     }
   }
@@ -39,7 +45,27 @@ export class PostsService {
   onDontLoveIt(postId:number) {
     if(this.posts[postId]) {
       this.posts[postId].loveIts--;
+      this.savePosts();
       this.emitPosts();
     }
+  }
+
+  getPosts() {
+    firebase.database().ref('/posts')
+      .on('value', (data: DataSnapshot) => {
+          if(!data.val()){
+            this.posts = this.defaultPosts.slice();
+            this.savePosts();
+            this.emitPosts();
+          } else {
+            this.posts = data.val();
+            this.emitPosts();
+          }
+        }
+      );
+  }
+
+  savePosts() {
+    firebase.database().ref('/posts').set(this.posts);
   }
 }
